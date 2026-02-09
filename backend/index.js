20dcc7c7078f876143aee9c9fae1d8881ada8fd2
Samuel Omondi <omondisam696@gmail.com>
2026-02-09 00:35:13 -0500

    package.json changes

--- backend/index.js
+++ backend/index.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
