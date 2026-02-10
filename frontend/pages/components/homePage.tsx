250624767b7fc57ebee890997269f5e3ebb2e57d
Samuel Omondi <omondisam696@gmail.com>
2026-02-10 00:44:30 -0500

    getting frontend hero set up

--- frontend/pages/components/homePage.tsx
+++ frontend/pages/components/homePage.tsx
@@ -1,9 +1,13 @@
+import Hero from "./hero";
 import Navbar from "./navbar";
 
 export default function HomePage() {
     return (
         <>
             <Navbar/>
+            <div style={{ textAlign: 'center', marginTop: '50px' }}>
+                <Hero/>
+            </div>
         </>
     )
 }
