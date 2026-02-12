a1828287df1f718124afd373b9504f7c0d7afd89
Samuel Omondi <omondisam696@gmail.com>
2026-02-11 22:56:48 -0500

    updated db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,7 @@ module.exports = mysql.createPool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
