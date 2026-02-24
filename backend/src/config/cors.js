10050d40a14baff13555864b319b55ea26c98c7f
Samuel Omondi <omondisam696@gmail.com>
2026-02-23 19:59:07 -0500

    testing cors

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,25 +1,28 @@
-const cors = require('cors');
+// const cors = require('cors');
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://staging-madi-visuals-2.vercel.app',
-].filter(Boolean); // removes undefined/null values
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://staging-madi-visuals-2.vercel.app',
+// ].filter(Boolean); // removes undefined/null values
 
-module.exports = cors({
-  origin: function (origin, callback) {
-    console.log('Incoming origin:', origin);
-    // Allow requests with no origin (Postman, mobile apps, curl)
-    if (!origin) return callback(null, true);
+// module.exports = cors({
+//   origin: function (origin, callback) {
+//     console.log('Incoming origin:', origin);
+//     // Allow requests with no origin (Postman, mobile apps, curl)
+//     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     }
 
-    return callback(new Error('Not allowed by CORS'));
-  },
+//     return callback(new Error('Not allowed by CORS'));
+//   },
+//   credentials: true,
+// });
+
+module.exports = require('cors')({
+  origin: true,
   credentials: true,
 });
-
-
 
 
