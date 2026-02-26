88c89dd515751685302f4b7687f1139cc5cb7b00
Samuel Omondi <omondisam696@gmail.com>
2026-02-25 22:14:49 -0500

    reset

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface ContactMessage {
