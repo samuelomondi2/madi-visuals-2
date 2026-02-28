f5fcfdd778a111c7580c1aef45a96abbf8e5b0e8
Samuel Omondi <omondisam696@gmail.com>
2026-02-28 00:56:44 -0500

    static upload

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const corsMiddleware = require('./src/config/cors');
 const bodyParser = require('body-parser');
+const path = require("path");
 
 const authRoutes = require('./src/routes/auth.route');
 const protectedRoutes = require('./src/routes/protected.route');
 const conctactRoute = require('./src/routes/contact.route')
 const heroRoute = require("./src/routes/hero.route");
-const uploadRoutes = require("./src/controller/upload.controller");
+const uploadRoutes = require("./src/routes/upload.route");
 const errorHandler = require("./src/middleware/errorHandler");
 
 const app = express();
@@ -21,8 +22,14 @@ app.use('/auth', authRoutes);
 app.use('/api', conctactRoute)
 app.use('/api', protectedRoutes);
 app.use('/api', heroRoute);
-app.use('/api', uploadRoutes);
+
+// Serve static files
+// Host Media on Your Own Server (Node.js Static Folder)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/upload", uploadRoutes);
+
 app.use(errorHandler);
+
 
 // Global error handler
 app.use((err, req, res, next) => {
