c70aa36143f7b5afc612f996c533ba403460da5c
Samuel Omondi <omondisam696@gmail.com>
2026-03-16 21:32:47 -0400

    service auth route

--- backend/src/routes/services.route.js
+++ backend/src/routes/services.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const controller = require("../controller/services.controller");
+const authenticateToken = require('../middleware/auth.middleware');
 
 const router = express.Router();
 
-router.get("/services", controller.getServices);
-router.get("/services/:id", controller.getService);
-router.post("/services", controller.addService);
-router.patch("/services/:id", controller.updateService);
+router.get("/services", authenticateToken, controller.getServices);
+router.get("/services/:id", authenticateToken, controller.getService);
+router.post("/services", authenticateToken, controller.addService);
+router.patch("/services/:id", authenticateToken, controller.updateService);
 
 module.exports = router;
