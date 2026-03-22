1e667b037a1394a8cbc8bbaac7ea9f6a20775b9a
Samuel Omondi <omondisam696@gmail.com>
2026-03-21 22:29:20 -0400

    bugfix: TypeError: argument handler must be a function

--- backend/src/routes/payments.route.js
+++ backend/src/routes/payments.route.js
@@ -1,12 +1,12 @@
-import express from "express";
-import { handleStripeWebhook } from "../controller/payments.controller";
+const express = require("express");
+const controller = require("../controller/payments.controller");
 
 const router = express.Router();
 
 router.post(
   "/webhook",
   express.raw({ type: "application/json" }),
-  handleStripeWebhook
+  controller.handleStripeWebhook
 );
 
 export default router;
