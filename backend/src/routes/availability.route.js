7946cab98a842c19fc40fe3f80c9ae19e9cb4215
Samuel Omondi <omondisam696@gmail.com>
2026-04-04 19:07:57 -0400

    bugfix

--- backend/src/routes/availability.route.js
+++ backend/src/routes/availability.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controller/availability.controller");
 
 router.get("/availability", controller.getAvailability);
-router.get("/admin/availability", controller.getAvailability);
+router.get("/admin/availability", controller.getAdminAvailability);
 router.post("/availability", controller.setAdminAvailability);           
 router.put("/availability/:day_of_week", controller.updateAvailability); 
 router.delete("/availability/:day_of_week", controller.deleteAvailability); 
