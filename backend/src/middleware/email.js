5fe8850dbdb0b8c1213fc1483a325c87ef521b81
Samuel Omondi <omondisam696@gmail.com>
2026-04-05 08:50:40 -0400

    bugfix

--- backend/src/middleware/email.js
+++ backend/src/middleware/email.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: Number(process.env.SMTP_PORT) === 465, 
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
