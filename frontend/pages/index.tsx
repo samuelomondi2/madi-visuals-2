0be94b155f8025c5fb0e9c5fc94d8974ca2a1b20
Samuel Omondi <omondisam696@gmail.com>
2026-06-04 00:38:35 -0400

    redirect commit

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
 import HomePage from "./components/homePage";
+import PageUnderConstruction from "./under-construction";
 
 export default function Home() {
   return (
     <>
-      <HomePage/> 
+      {/* <HomePage/>  */}
+      <PageUnderConstruction/>
     </>
   );
 }
