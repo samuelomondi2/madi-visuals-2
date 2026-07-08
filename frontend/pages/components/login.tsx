ffd3e117f3ac9d17e50461b79608584d9876d7c0
Samuel Omondi <omondisam696@gmail.com>
2026-07-07 21:35:48 -0400

    forgot password

--- frontend/pages/components/login.tsx
+++ frontend/pages/components/login.tsx
@@ -11,6 +11,7 @@ export default function LoginPage() {
     e.preventDefault();
     setError("");
 
+    console.log("Login URL:", `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`);
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
         method: "POST",
